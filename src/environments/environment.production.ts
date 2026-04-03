19f1b2365109e93b3e61376fe280cd02e02baf99
Alexander H Lee III <ahlee1026@gmail.com>
2026-04-02 20:29:57 -0400

    update openweather api key

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  openWeatherApiKey: 'YOUR_OPENWEATHER_API_KEY',
+  openWeatherApiKey: '9b70bb67f4b51dda59df2e5423edd10f',
   oneCallBaseUrl: 'https://api.openweathermap.org/data/3.0',
   geocodingBaseUrl: 'https://api.openweathermap.org/geo/1.0',
 };
